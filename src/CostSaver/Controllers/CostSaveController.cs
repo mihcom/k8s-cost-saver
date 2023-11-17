b6f846f5e2bcd545413510ba736e1562b3fdeb7a
Mykhaylo Gomonyuk <mgo@leapwork.com>
2023-11-17 15:36:07 +0100

    WIP

--- src/CostSaver/Controllers/CostSaveController.cs
+++ src/CostSaver/Controllers/CostSaveController.cs
@@ -1,8 +1,26 @@
 ﻿using KubeOps.Operator.Controller;
+using KubeOps.Operator.Controller.Results;
 
 namespace CostSaver.Controllers;
 
 public class CostSaveController : IResourceController<Entities.CostSaver>
 {
-    
+    private readonly ILogger<CostSaveController> _logger;
+
+    public CostSaveController(ILogger<CostSaveController> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<ResourceControllerResult?> ReconcileAsync(Entities.CostSaver entity)
+    {
+        _logger.LogInformation("Reconciling {EntityName} in namespace {Namespace}.", entity.Metadata.Name, entity.Metadata.NamespaceProperty);
+        return Task.FromResult((ResourceControllerResult?)null);
+    }
+
+    public Task DeletedAsync(Entities.CostSaver entity)
+    {
+        _logger.LogInformation("Deleting {EntityName} in namespace {Namespace}.", entity.Metadata.Name, entity.Metadata.NamespaceProperty);
+        return Task.CompletedTask;
+    }
 }
